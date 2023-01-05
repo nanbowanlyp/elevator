b3cb7111e0c92df4bda1b03fad3a1b64393aba6d
Funder666 <841610078@qq.com>
2023-01-05 22:21:00 +0800

    修改了返回值类型

--- hafuman/Mysql.h
+++ hafuman/Mysql.h
@@ -1,5 +1,6 @@
 #pragma once
+#include <mysql.h>
 void Connect();
-void Select();
+MYSQL_RES* Select();
 void Insert(int id, int start, int now, int end, char direction[20], char state[20]);
 void Delete(int delete_id);
